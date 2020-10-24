ea0e20066fc18754534cc5129eb3a33ca780ccb7
Kiran LM <kiran.lm96@gmail.com>
2020-10-24 20:58:15 +0530

    circletype, three bg animation change and other design changes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,21 @@
 import { h } from "preact";
 import { Link } from "preact-router/match";
 import style from "./style.css";
-
-const Header = () => (
-  <div className="title-wrapper">
-    <h1>
-      <i className="fa fa-hand-peace-o" aria-hidden="true"></i> Kiran LM ...
-    </h1>
-  </div>
-);
+import { useEffect } from "preact/hooks";
+import CircleType from "circletype";
+const Header = () => {
+  useEffect(() => {
+    const circleType = new CircleType(document.getElementById("circle-text"));
+    circleType.dir(-1);
+  }, []);
+  return (
+    <div className="title-wrapper">
+      <h1>
+        <i className="fa fa-hand-peace-o" aria-hidden="true"></i> Hello ...
+      </h1>
+      <div id="circle-text">RUN - EAT - CODE - SLEEP - REPEAT - </div>
+    </div>
+  );
+};
 
 export default Header;
--- src/components/RightPanel/index.js
+++ src/components/RightPanel/index.js
@@ -1,5 +1,25 @@
 import { h } from "preact";
 
-const RightPanel = () => <div className="col-sm-12 col-md-5 right"></div>;
+const RightPanel = () => {
+  return (
+    <>
+      <div className="col-sm-12 col-md-5 right"></div>
+      <div className="icon-container">
+        <div className="icon-wrapper">
+          <i class="fab fa-github"></i>
+        </div>
+        <div className="icon-wrapper">
+          <i class="fab fa-linkedin-in"></i>
+        </div>
+        <div className="icon-wrapper">
+          <i class="fab fa-twitter"></i>
+        </div>
+        <div className="icon-wrapper">
+          <i class="fab fa-instagram"></i>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default RightPanel;
